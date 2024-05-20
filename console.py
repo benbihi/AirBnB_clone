9e3485b918ea35e19bca901389796789f4b60913
benbihi <elm.benbihi@gmail.com>
2024-05-19 17:25:43 -0700

    do create, all done

--- console.py
+++ console.py
@@ -19,5 +19,41 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing on empty input line"""
         pass
 
+    def do_create(self, arg):
+        """Usage: create <class>
+        Create a new class instance and print its id.
+        """
+        argl = parse(arg)
+        if len(argl) == 0:
+            print("** class name missing **")
+        elif argl[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argl[0])().id)
+            storage.save()
+
+    def do_show(self, arg):
+        pass
+
+    def do_destroy(self, arg):
+        pass
+
+    def do_all(self, arg):
+        if args:
+            args = args.split(' ')[0]
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage.all().items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
+        else:
+            for k, v in storage.all().items():
+                print_list.append(str(v))
+
+    def do_update(self, arg):
+        pass
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
